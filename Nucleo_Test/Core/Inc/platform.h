2685c1835c180921e50e8d440395de445991d523
Hussein Arafa <arafahusseinn@gmail.com>
2025-10-07 23:26:16 +1030

    First commit

---
+++ Nucleo_Test/Core/Inc/platform.h
@@ -0,0 +1,12 @@
+#ifndef PLATFORM_H
+#define PLATFORM_H
+
+#include <stdint.h>
+#include <string.h>
+
+int32_t platform_write(void *handle, uint8_t reg, const uint8_t *bufp, uint16_t len);
+int32_t platform_read(void *handle, uint8_t reg, uint8_t *bufp, uint16_t len);
+void platform_delay(uint32_t ms);
+void platform_write_debug(char *msg);
+
+#endif
